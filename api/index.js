0fb84890ed26cadf337f3286b952fb21fa37c339
marcosr1 <marcosrichelly43@gmail.com>
2026-03-01 22:42:29 -0300

    test4

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-import app from "../src/app.js";
+import { handleDenuncia } from "../src/routes/denunciaRoutes.js";
 
-export default function handler(req, res) {
-   try {
-      return app(req, res);
-      
-   } catch ( error ) {
+export default async function handler(req, res) {
+  try {
+    // Chama a função da rota diretamente
+    await handleDenuncia(req, res);
+  } catch (error) {
     console.error("Erro na função:", error);
     res.status(500).json({ erro: "Erro interno do servidor" });
-   }
+  }
 }
