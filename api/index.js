4bff0fcba3464843a88194dc47c84ed6eb1438c9
marcosr1 <marcosrichelly43@gmail.com>
2026-03-01 22:31:23 -0300

    test2

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import app from "../src/app.js";
 
 export default function handler(req, res) {
-  return app(req, res);
+   try {
+      return app(req, res);
+   } catch ( error ) {
+    console.error("Erro na função:", error);
+    res.status(500).json({ erro: "Erro interno do servidor" });
+   }
 }
